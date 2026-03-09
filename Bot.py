fef419f58a88e0afb99c98582e3d3a38ecd92db2
Obito Uchiha <115386722+Obito1984@users.noreply.github.com>
2026-03-09 16:39:36 +0530

    Bot.py

--- Bot.py
+++ Bot.py
@@ -3,17 +3,36 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8638803031:AAFcqafHFSD_hUTobO0oknmeEsfK4vphkyc"
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is online ✅")
+
 async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-    "📜 HeavenFall Rules\n\n• No NSFW spam\n• Respect members\n• No scam links")
+"""📜 HeavenFall Rules
+
+• No NSFW spam
+• No abusive language
+• No scam links
+• Respect all members
+""")
 
 async def network(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-    "🌐 HeavenFall Network\n\nMain Channel: https://t.me/HeavenFallNetwork")
+"""🌐 HeavenFall Network
+
+Main Channel:
+https://t.me/HeavenFallNetwork
+
+Discussion Group:
+https://t.me/heavenfalldiscuss
+""")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("rules", rules))
 app.add_handler(CommandHandler("network", network))
+
+print("Bot started...")
 
 app.run_polling()
